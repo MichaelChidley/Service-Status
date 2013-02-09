ab1bea1cfb0bcbda275e1d68aeca4002d6e7ed10
MichaelChidley <michael24687@hotmail.com>
2013-02-09 12:25:08 +0000

    fixed bugs and added commenting

--- serviceStatus.sh
+++ serviceStatus.sh
@@ -11,31 +11,36 @@
 #Grab the first argument so this script is universal 
 SERVICE=$1
 
+#Setup what services can be checks by using part of a string that is
+#returned when service SERVICENAME status is executed
 if [ "$SERVICE" == "apache2" ]; then
         TOGREP="pid"
 elif [ "$SERVICE" == "mysql" ]; then
-        TOGREP="start/running"
+        TOGREP="process"
 elif [ "$SERVICE" == "vsftpd" ]; then
         TOGREP="start/running"
 fi 
+
   
 #Email address to notify
-EMAIL="";
+EMAIL=""
 
-#If the status of the service is running it will return a PID, so check that
+#If the status of the service is running it will return whats defined above
 if service $SERVICE status | grep "$TOGREP"
 then
-        echo ""
+        return 0
 
 #if it is not found, initiate a restart of the service, and notify email address of the result
 else
         #Initiate a start of the service and apply message content to the variable with the result   
-        if service $SERVICE start   
+        if service $SERVICE start  
         then
                 RESULT="$SERVICE was found not to be running but successfully restarted!"
         else
                 RESULT="$SERVICE was unable to start!"
         fi
+        
+        echo $RESULT
 
         #Email the defined email address with the notice and results of restart
         echo "$RESULT" | mutt -s "$SERVICE STATUS" -- $EMAIL     
